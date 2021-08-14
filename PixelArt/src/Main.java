33808e0b5cd3815bb4714d10864872f0aa0c8c96
RuiCunha96 <81982415+RuiCunha96@users.noreply.github.com>
2021-08-14 15:33:45 +0100

    Update Main.java
    added try-catch

--- PixelArt/src/Main.java
+++ PixelArt/src/Main.java
@@ -2,9 +2,16 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Inputs input = new Inputs();
 
-        Grid grid = new Grid();
+         try {
+            Grid grid = new Grid();
+        } catch (IOException e) {
+            System.out.println("Something happened...");
+        }
+    }
+        
+    
     }
 }
